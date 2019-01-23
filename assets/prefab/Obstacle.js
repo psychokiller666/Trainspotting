11515691fb51a991aaa8ddd6396df474efd90d68
psychokiller <fucked@psychokiller.me>
2019-01-24 02:00:28 +0800

    随机金币

--- assets/prefab/Obstacle.js
+++ assets/prefab/Obstacle.js
@@ -26,31 +26,29 @@ cc.Class({
 
     onLoad: function () {
         this.id = _.random(1, 5)
+        this.gooditem = null
         this.init()
     },
     
-    init: function () {
-        this.node.height = Config.OBSTACLE_ITEM[this.id].size.height
-
-        this.node.obstacle_left.$Widget.left = 0
-        this.node.obstacle_right.$Widget.right = 0
-
-        this.node.obstacle_left.$ObstacleItem.id = this.id
-        this.node.obstacle_right.$ObstacleItem.id = this.id
-
+    start: function () {
         // 生成金币
         for (let i = 1; GOOD_ITEM[1].quantity >= i; i++) {
-            let good = cc.instantiate(this.goodItem)
-            this.node.addChild(good)
+            this.gooditem = cc.instantiate(this.goodItem)
+            
 
-            switch (GOOD_ITEM[2].position){
+            switch (GOOD_ITEM[1].position){
                 case 'left':
-                    good.getComponent(cc.Widget).isAlignRight = false
-                    good.getComponent(cc.Widget).left = Config.OBSTACLE_ITEM[this.id].size.width
-                    good.getComponent(cc.Widget).isAlignLeft = true
+                    this.gooditem.setPosition(- (375 - Config.OBSTACLE_ITEM[this.id].size.width) + ( this.gooditem.width / 2), 20)
+                    // good.x = 
+                    // good.y = - 
+                    // console.log(Config.OBSTACLE_ITEM[this.id].size.width)
+                    // good.getComponent(cc.Widget).isAlignRight = false
+                    // good.getComponent(cc.Widget).Target = this.node
+                    // good.getComponent(cc.Widget).left = Config.OBSTACLE_ITEM[this.id].size.width
+                    // good.getComponent(cc.Widget).isAlignLeft = true
 
-                    good.getComponent(cc.Widget).top = i * good.height + 20
-                    good.getComponent(cc.Widget).isAlignTop = true
+                    // good.getComponent(cc.Widget).top = 0
+                    // good.getComponent(cc.Widget).isAlignTop = true
 
                     // good.getComponent(cc.Widget).updateAlignment()
                 break;
@@ -61,9 +59,21 @@ cc.Class({
                     
                 break;
             }
-            // console.log(good.getComponent(cc.Widget))
-            console.log(good.getComponent(cc.Widget).top)
-        }
 
+            this.node.addChild(this.gooditem)
+
+            // console.log(good.y)
+        }
+    },
+
+    init: function () {
+        this.node.height = Config.OBSTACLE_ITEM[this.id].size.height
+
+        this.node.obstacle_left.$Widget.left = 0
+        this.node.obstacle_right.$Widget.right = 0
+
+        this.node.obstacle_left.$ObstacleItem.id = this.id
+        this.node.obstacle_right.$ObstacleItem.id = this.id
+        // this.createGood()
     }
 });
